616e5aaed3e3c0a4062c18647a44149e8b8ec73f
Danila Finkt <d.finkt@yandex.com>
2025-03-24 21:49:39 +0300

    3

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
 
 from cats.views import CatViewSet, OwnerViewSet
 
@@ -11,4 +12,9 @@ router.register('owners', OwnerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    # Djoser создаст набор необходимых эндпоинтов.
+    # базовые, для управления пользователями в Django:
+    path('auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('auth/', include('djoser.urls.jwt')),
 ]
